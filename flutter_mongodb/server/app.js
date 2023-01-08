271054db836de49f7a45812d5ca1342f96e11fb9
Salih Kaan Koç <72124091+salihkaankoc@users.noreply.github.com>
2023-01-08 16:27:21 +0300

    Update app.js

--- flutter_mongodb/server/app.js
+++ flutter_mongodb/server/app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(authRoute);
 
-mongoose.connect(`mongodb+srv://admin:admin123@cluster0.lzqiefj.mongodb.net/?retryWrites=true&w=majority`, {autoIndex : false}).then(() => {
+mongoose.connect(`YOURAPPLINK`, {autoIndex : false}).then(() => {
     app.listen(5000);
     console.log('Bağlantı Başarılı!')
 }).catch(err => {
